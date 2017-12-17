66d91aa28f4a798ea98cf88d1b892a21a6e69d6d
Shekhar Gyanwali <saykharng@hotmail.com>
2017-12-18 00:47:18 +1100

    last func left

--- 03/tags-help.py
+++ 03/tags-help.py
@@ -2,25 +2,37 @@ from collections import Counter
 from difflib import SequenceMatcher
 from itertools import product
 import re
+import pdb
 
 IDENTICAL = 1.0
 TOP_NUMBER = 10
 RSS_FEED = 'rss.xml'
 SIMILAR = 0.87
 TAG_HTML = re.compile(r'<category>([^<]+)</category>')
+tag_html = '<.*?>'
 
 
 def get_tags():
     """Find all tags (TAG_HTML) in RSS_FEED.
     Replace dash with whitespace.
     Hint: use TAG_HTML.findall"""
-    pass
+    with open(RSS_FEED, 'r') as rss:
+        for line in rss:result = TAG_HTML.findall(line)
 
+    for item in result:
+        if '-' in item:result[result.index(item)] = ' '.join(item.split('-'))
+    all_tags = [item for item in result]
+    return all_tags
 
 def get_top_tags(tags):
     """Get the TOP_NUMBER of most common tags
     Hint: use most_common method of Counter (already imported)"""
-    pass
+    all_tags = Counter()
+    for item in tags:
+        all_tags[item] += 1
+    #pdb.set_trace()
+    TOP_NUMBER = all_tags.most_common()[0][1]
+    return all_tags.items()
 
 
 def get_similarities(tags):
@@ -28,6 +40,8 @@ def get_similarities(tags):
     Hint 1: compare each tag, use for in for, or product from itertools (already imported)
     Hint 2: use SequenceMatcher (imported) to calculate the similarity ratio
     Bonus: for performance gain compare the first char of each tag in pair and continue if not the same"""
+    
+    
     pass
 
 
