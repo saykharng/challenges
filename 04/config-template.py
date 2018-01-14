aa6e930a850e50ea488cfd6a9cf27e1ea12905cd
Shekhar Gyanwali <saykharng@hotmail.com>
2018-01-14 23:23:43 +1100

    filter_crap is being re-written

--- 04/config-template.py
+++
@@ -1,5 +0,0 @@
-# Getting your API keys from Twitter - https://apps.twitter.com
-CONSUMER_KEY = '8JpOdh5D8ufSHm9uEZLmDEnpi'
-CONSUMER_SECRET = 'uGCzK2LxvseEPoecYM84TeR7EOdnziopJLVEoxkDKaSbKNLBD5'
-ACCESS_TOKEN = '3247378184-yLm1Iyo8aiI4BKHV4YSzuUzb6cWz5n4QT61Zc2Z'
-ACCESS_SECRET = 'h4I9oj2yaGqsyd0wh7kBrMELI8oHhMuVngDMsMg8YCxjA'
--- 05/similar_tweeters.py
+++ 05/similar_tweeters.py
@@ -1,5 +1,5 @@
 import sys
-from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.tokenize import sent_tokenize, word_tokenize, RegexpTokenizer, TweetTokenizer
 from usertweets import UserTweets
 from nltk.corpus import stopwords
 
@@ -8,27 +8,34 @@ def similar_tweeters(user1, user2):
     user2_tweets = UserTweets(user2)
     user1_token = []
     for tweets in user1_tweets._tweets:
-        user1_token.append(word_tokenize(tweets[2]))
-        
+        print(''.join(tweets[2]))
+        #user1_token.append(word_tokenize(''.join(tweets[2])))
+    #print(user1_token)    
+    #filter_crap(user1_token) 
 
     
 def filter_crap(tweet_tokens):
     #remove stopwords
-    stop = stopwords('english')
-    result = [words for words in tweet_tokens if words not in stop]
-                    
+    stop = stopwords.words('english')
+    result = []
+    for item in tweet_tokens:
+        result.append([words for words in item if words not in stop])
+    print(result) 
     #remove urls
     
     #remove digits
-    result = [words for words in result if words.isdigit()]
+    #for item in result:
+    #   result = [words for words in item if words.isdigit()]
 
     #remove puntuation
-    
+    tokenizer = RegexpTokenizer(r'\w+')
+    result = [tokenizer.tokenize(words) for words in result]
+    #print(result)  
     #remove words which occurs only once
     result = [words for words in result if result.count(words) < 2]
     #remove words with less than 3 char
     result = [words for words in result if len(words) < 3]
-
+    #print(result)
     
 if __name__ == "__main__":
 
