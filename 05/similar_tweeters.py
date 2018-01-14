84c0c1308b8f61ffd0599461c097c344a1a936ea
Shekhar Gyanwali <saykharng@hotmail.com>
2018-01-14 21:41:40 +1100

    filter_crap is written

--- 05/similar_tweeters.py
+++ 05/similar_tweeters.py
@@ -1,6 +1,7 @@
 import sys
 from nltk.tokenize import sent_tokenize, word_tokenize
 from usertweets import UserTweets
+from nltk.corpus import stopwords
 
 def similar_tweeters(user1, user2):
     user1_tweets = UserTweets(user1)
@@ -8,9 +9,27 @@ def similar_tweeters(user1, user2):
     user1_token = []
     for tweets in user1_tweets._tweets:
         user1_token.append(word_tokenize(tweets[2]))
+        
 
     
+def filter_crap(tweet_tokens):
+    #remove stopwords
+    stop = stopwords('english')
+    result = [words for words in tweet_tokens if words not in stop]
+                    
+    #remove urls
+    
+    #remove digits
+    result = [words for words in result if words.isdigit()]
 
+    #remove puntuation
+    
+    #remove words which occurs only once
+    result = [words for words in result if result.count(words) < 2]
+    #remove words with less than 3 char
+    result = [words for words in result if len(words) < 3]
+
+    
 if __name__ == "__main__":
 
     if len(sys.argv) < 3:
