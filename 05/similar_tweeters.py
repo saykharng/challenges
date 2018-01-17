1688b63059bb456a50ce7a4186cc849a25776a87
Shekhar Gyanwali <saykharng@hotmail.com>
2018-01-17 21:32:17 +1100

    filter_crap now returns processed tweet with high value word

--- 05/similar_tweeters.py
+++ 05/similar_tweeters.py
@@ -11,7 +11,6 @@ def similar_tweeters(user1, user2):
     user1_token = []
     for tweets in user1_tweets._tweets:
         user1_token.append(tknzr.tokenize(tweets[2]))
-      
     filter_crap(user1_token) 
 
     
@@ -21,24 +20,19 @@ def filter_crap(tweet_tokens):
     result = []
     for item in tweet_tokens:
         result.append([words for words in item if words not in stop])
-    #print(result) 
-    #remove urls
-    
-    #remove digits
-    #for item in result:
-    #   result = [words for words in item if words.isdigit()]
-
     #remove puntuation
     tokenizer = RegexpTokenizer(r'\w+')
     result = [tokenizer.tokenize(words) for words in list(chain.from_iterable(result))]
-    #print(list(chain.from_iterable(result))  )
     #remove words which occurs only once
     temp_list = list(chain.from_iterable(result))
     result = [words for words in temp_list if temp_list.count(words) > 1]
-    print(result)
+    #remove URL from tweets
+    result = [words for words in result if 'http' not in words]
+    #remove digits
+    result = [words for words in result if words.isalpha()]
     #remove words with less than 3 char
-    result = [words for words in result if len(words) < 3]
-    #print(result)
+    result = [words for words in result if len(words) > 2]
+    return result
     
 if __name__ == "__main__":
 
